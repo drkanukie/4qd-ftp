1920d5ed68724c8be9045f80366ef1d7204cb165
drkanukie <drkanukie@gmail.com>
2016-11-24 00:56:09 +0000

    new social icons

--- themes/fourqd-child/x-footer.php
+++ themes/fourqd-child/x-footer.php
@@ -47,6 +47,11 @@
         <br>
         </p>
         <p><a href="http://www.4qd.co.uk/terms-and-conditions/">Terms and Conditions</a></p>
+        <!--div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script -->
       </div>
       <div id="footerform" >
         <?php echo apply_filters('the_content','[si-contact-form form="1"]');?>
